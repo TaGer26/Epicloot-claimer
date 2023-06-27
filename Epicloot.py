52b9dc2cba6dd4e3957ac93800cc5d6fa3762313
TaGer26 <69417268+TaGer26@users.noreply.github.com>
2023-06-27 20:11:28 +0500

    v1.1
    - error fix

--- Epicloot.py
+++ Epicloot.py
@@ -2,13 +2,34 @@ import time
 import threading
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import WebDriverException
 
 bonus = 0
 element = 0
 
+##Opt
+options = webdriver.ChromeOptions()
+# options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--mute-audio')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+options.add_argument('--disable-infobars')
+options.add_argument('--ignore-certificate-errors-spki-list')
+options.add_argument('--no-sandbox')
+options.add_argument('--no-zygote')
+options.add_argument('--log-level=3')
+options.add_argument('--allow-running-insecure-content')
+options.add_argument('--disable-web-security')
+options.add_argument('--disable-features=VizDisplayCompositor')
+options.add_argument('--disable-breakpad')
+
 ##Func
 def get1():
     global bonus
+    global options
     global element
     wd.refresh()
     f_element()
@@ -21,6 +42,7 @@ def get1():
 t1 = threading.Thread(target=get1)
 
 def f_element():
+    global options
     global element
     time.sleep(7)
     element = wd.find_element(By.CLASS_NAME, 'game-gift__take')
@@ -28,7 +50,7 @@ def f_element():
 ##Start
 print ('Epicloot-event-claimer')
 print ('')
-wd = webdriver.Chrome()
+wd = webdriver.Chrome(options=options)
 wd.get('http://epicloot.in')
 time.sleep(0.25)
 st = input('Пожалуйста авторизируйтесь в аккаунт и перейдите на страницу event, затем нажмите enter: ')
